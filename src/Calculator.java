14137aeb1d401126fbf38840f4c891569b336927
Aadam Zaman <Aadam.Zaman@mail.bcu.ac.uk>
2018-11-05 12:33:14 +0000

    pair programming lab work

---
+++ src/Calculator.java
@@ -0,0 +1,34 @@
+
+public class Calculator {
+
+	public void loadConstant(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void total() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
